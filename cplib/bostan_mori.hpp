53b9893041018c3a8acb3def238608a7b6eb71ad
Joe75792433 <67582330+Joe75792433@users.noreply.github.com>
2025-12-23 22:02:56 +0900

    add

--- cplib/bostan_mori.hpp
+++ cplib/bostan_mori.hpp
@@ -1,5 +1,4 @@
-#ifndef CPLIB_BOSTAN_MORI_HPP
-#define CPLIB_BOSTAN_MORI_HPP 1
+#pragma once
 
 #include <cassert>
 #include <ranges>
@@ -8,12 +7,12 @@
 namespace cplib {
 
 // 有理式 p(x)/q(x) のn次の係数を求める
-// deg(p) < deg(q) であること
+// deg(p) < deg(q) かつ q[0] != 0 であること
 template <typename T>
 T bostan_mori(unsigned long long n,
               std::vector<T> p,
               std::vector<T> q,
-              auto convolution_func) {
+              const auto& convolution_func) {
     while (n > 0) {
         const size_t len_q = q.size();
         std::vector<T> q_ = q;
@@ -32,5 +31,3 @@ T bostan_mori(unsigned long long n,
 }
 
 }  // namespace cplib
-
-#endif  // CPLIB_BOSTAN_MORI_HPP
